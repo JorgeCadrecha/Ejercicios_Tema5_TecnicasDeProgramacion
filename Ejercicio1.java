be3b9dc689c2a57eb956ba36863c5ef2c4e224db
JorgeCadrecha <113858332+JorgeCadrecha@users.noreply.github.com>
2023-02-22 14:12:36 +0000

    Ejercicio1,10

---
+++ Ejercicio1.java
@@ -0,0 +1,6 @@
+/*
+* 1. Despegue inmediato
+Crear un algoritmo escribiendo los valores comprendidos entre diez y cero en orden descendente en una tabla, y posteriormente recorrer esta tabla para mostrar la cuenta hacia atrás.
+ */
+
+ 
